da357c8087cc8ad745408088aefe546cd68ee0ad
EasymoneyF <55006262+EasymoneyF@users.noreply.github.com>
2020-09-01 11:32:19 -0700

    api outputted to table on addstock

--- usstocks/market_data/urls.py
+++ usstocks/market_data/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about.html', views.about, name="about"),
     path('add_stock.html', views.add_stock, name="add_stock"),
+    path('delete/<stock_id>', views.delete, name="delete"),
 
 ]
--- usstocks/market_data/views.py
+++ usstocks/market_data/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Stock
+from django.contrib import messages
+from .forms import StockForm
+
 
 def index(request):
     import requests
@@ -21,11 +24,35 @@ def index(request):
 
 
 
-
-
 def about(request):
     return render(request, 'about.html', {})
 
 def add_stock(request):
-    stock_quote = Stock.objects.all()
-    return render(request, 'add_stock.html', {'stock_quote': stock_quote})
+    import requests
+    import json
+
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock Has Been Added"))
+            return redirect("add_stock")
+    else:
+        stock_quote = Stock.objects.all()
+        output = []
+        for stock_quote_item in stock_quote:
+            api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + str(stock_quote_item) + "/quote?token=pk_cd665dda2a6d453f90863dd2e47a45fc")
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Ticker Name Error..."
+        return render(request, 'add_stock.html', {'stock_quote': stock_quote, 'output': output})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock Has Been Deleted"))
+    return redirect(add_stock)
